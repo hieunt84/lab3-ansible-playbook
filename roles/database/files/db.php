dc51378934d79c38e18a4b877e0e28a212b94cfe
MinhNPH <hieunt9@example.com>
2021-03-20 10:55:30 +0700

    edit file db.php in roles/database/files/

--- roles/database/files/db.php
+++ roles/database/files/db.php
@@ -1,27 +1,19 @@
 <?php
 // connect database and quyery data and output
-$connection = new PDO('mysql:host=localhost;dbname=demo', 'demo', 'demo');
-$statement  = $connection->query('SELECT message FROM demo');
-
-echo $statement->fetchColumn();
-?>
-
-<?php
-// connect database and quyery data and output
-$user = "example_user";
-$password = "password";
-$database = "example_database";
-$table = "todo_list";
+//$connection = new PDO('mysql:host=localhost;dbname=demo', 'demo', 'demo');
+//$statement  = $connection->query('SELECT message FROM demo');
+//echo $statement->fetchColumn();
 
 try {
-  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-  echo "<h2>TODO</h2><ol>";
-  foreach($db->query("SELECT content FROM $table") as $row) {
-    echo "<li>" . $row['content'] . "</li>";
+    $db = new PDO('mysql:host=localhost;dbname=demo', 'demo', 'demo');
+    echo "<h2>TODO</h2><ol>";
+    foreach($db->query('SELECT message FROM demo') as $row) {
+      echo "<li>" . $row['message'] . "</li>";
+    }
+    echo "</ol>";
+  } catch (PDOException $e) {
+      print "Error!: " . $e->getMessage() . "<br/>";
+      die();
   }
-  echo "</ol>";
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
+
 ?>
